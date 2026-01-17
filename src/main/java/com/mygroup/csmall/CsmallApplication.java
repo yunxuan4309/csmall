3fcc255c8f45ba56d1b666bfd817536fb6244bd9
17214 <1721476339@qq.com>
2026-01-17 21:15:25 +0800

    chore:项目初始架构上传 -项目初始架构上传---预计将使用微服务架构来实现

---
+++ src/main/java/com/mygroup/csmall/CsmallApplication.java
@@ -0,0 +1,13 @@
+package com.mygroup.csmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mygroup/csmall/CsmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mygroup.csmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
