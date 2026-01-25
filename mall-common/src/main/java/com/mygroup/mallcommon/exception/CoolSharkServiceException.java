c3edcd2d0a95fc9454df2ce6343fba6025cb1173
17214 <1721476339@qq.com>
2026-01-25 22:25:05 +0800

    feat(module): 新增公共组件模块,包括: -通用响应封装 -异常处理机制 -JWT认证工具 -自定义认证框架数据封装类 -缓存键前缀配置规范和正则表达式接口

---
+++ mall-common/src/main/java/com/mygroup/mallcommon/exception/CoolSharkServiceException.java
@@ -0,0 +1,21 @@
+package com.mygroup.mallcommon.exception;
+
+import com.mygroup.mallcommon.restful.ResponseCode;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 业务异常
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class CoolSharkServiceException extends RuntimeException {
+
+    private ResponseCode responseCode;
+
+    public CoolSharkServiceException(ResponseCode responseCode, String message) {
+        super(message);
+        setResponseCode(responseCode);
+    }
+
+}
---
+++ mall-common/src/main/java/com/mygroup/mallcommon/validation/handler/BindExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.mygroup.mallcommon.validation.handler;
+
+import com.mygroup.mallcommon.restful.JsonResult;
+import com.mygroup.mallcommon.restful.ResponseCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class BindExceptionHandler {
+
+    @ExceptionHandler(BindException.class)
+    public JsonResult handleBindException(BindException e) {
+        log.debug("验证请求数据时出现异常：{}", e.getClass().getName());
+        e.printStackTrace();
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+        JsonResult result = JsonResult.failed(ResponseCode.BAD_REQUEST, message);
+        log.debug("即将返回：{}", result);
+        return result;
+    }
+
+}
