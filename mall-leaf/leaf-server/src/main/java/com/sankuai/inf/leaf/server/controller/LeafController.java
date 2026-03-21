d4ad4d6c6f9720213f7d31725d88eec41941b8ba
17214 <1721476339@qq.com>
2026-03-21 15:49:00 +0800

    feat(module): 新增分布式id生成模块

---
+++ mall-leaf/leaf-server/src/main/java/com/sankuai/inf/leaf/server/controller/LeafController.java
@@ -0,0 +1,49 @@
+package com.sankuai.inf.leaf.server.controller;
+
+import com.sankuai.inf.leaf.common.Result;
+import com.sankuai.inf.leaf.common.Status;
+import com.sankuai.inf.leaf.server.exception.LeafServerException;
+import com.sankuai.inf.leaf.server.exception.NoKeyException;
+import com.sankuai.inf.leaf.server.service.SegmentService;
+import com.sankuai.inf.leaf.server.service.SnowflakeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LeafController {
+    private Logger logger = LoggerFactory.getLogger(LeafController.class);
+
+    @Autowired
+    private SegmentService segmentService;
+    @Autowired
+    private SnowflakeService snowflakeService;
+    //http://localhost:9090/api/segment/get/admin 调用一下试试看
+    //http://localhost:9090/cache 查看一下控制台
+    //IdGeneratorUtils 这个类,专门用来访问leaf;
+    @RequestMapping(value = "/api/segment/get/{key}")//这个key指的是数据库里面的key1字段
+    public String getSegmentId(@PathVariable("key") String key) {
+
+        return get(key, segmentService.getId(key));
+    }
+
+    @RequestMapping(value = "/api/snowflake/get/{key}")
+    public String getSnowflakeId(@PathVariable("key") String key) {
+        return get(key, snowflakeService.getId(key));
+    }
+
+    private String get(@PathVariable("key") String key, Result id) {
+        Result result;
+        if (key == null || key.isEmpty()) {
+            throw new NoKeyException();
+        }
+        result = id;
+        if (result.getStatus().equals(Status.EXCEPTION)) {
+            throw new LeafServerException(result.toString());
+        }
+        return String.valueOf(result.getId());
+    }
+}
